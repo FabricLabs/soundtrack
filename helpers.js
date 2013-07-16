325473e5197e0d57c553f3af3f9440d66cacceb8
Rob Riddle <unusualbob@gmail.com>
2013-07-16 13:17:00 -0400

    undo link fixes

--- helpers.js
+++ helpers.js
@@ -18,12 +18,7 @@ exports.helpers = {
   prepareChatMessage: function(message) {
     return _.map(message.split(" "), function (element) {
       if (MAGIC_REGEX.test(element)) {
-        var httpelement = element;
-        //prevents urls from linking to soundtrack
-        if (httpelement.substring(0,4) != 'http') {
-          httpelement = 'http://' + httpelement;
-        }
-        return '<a href=' + httpelement + ' target="_blank">' + _.escape(element) + '</a>'
+        return '<a href=' + element + ' target="_blank">' + _.escape(element) + '</a>'
       } else {
         return _.escape(element)
       }
