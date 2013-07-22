3216731cf585f14539595f08fb8fafe6079422fa
Eric Martindale <eric@ericmartindale.com>
2013-07-22 08:38:07 +0000

    Fix restler data.

--- soundtrack.js
+++ soundtrack.js
@@ -176,8 +176,8 @@ function getYoutubeVideo(videoID, callback) {
         // it'll be slow.
         rest.get('http://codingsoundtrack.org/songs/1:'+video.id+'.json').on('complete', function(data) {
 
-          console.log(data);
-          if (!data) {
+          console.log('fff: ' + data);
+          if (!data.author) {
             data = {
               author: video.title.split(' - ')[0]
             };
