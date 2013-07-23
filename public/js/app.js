31ab528e8c4ec6cbee3922a5b5779997691663d7
Eric Martindale <eric@ericmartindale.com>
2013-07-23 02:28:22 +0000

    Allow video to resize responsively.

--- public/js/app.js
+++ public/js/app.js
@@ -199,7 +199,7 @@ $(window).on('load', function() {
   var atts = { id: "ytPlayer" };
 
   // All of the magic handled by SWFObject (http://code.google.com/p/swfobject/)
-  swfobject.embedSWF("http://www.youtube.com/apiplayer?version=3&enablejsapi=1&playerapiid=player1", "screen-inner", "570", "295", "9", null, null, params, atts);
+  swfobject.embedSWF("http://www.youtube.com/apiplayer?version=3&enablejsapi=1&playerapiid=player1", "screen-inner", "100%", "295", "9", null, null, params, atts);
 
   $('*[data-action=toggle-volume]').click(function(e) {
     e.preventDefault();
