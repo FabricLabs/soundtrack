0ac78acd8cde889d2a6de79dc9af1e4acac9b31c
Eric Martindale <eric@ericmartindale.com>
2013-11-11 15:19:13 +0000

    such good /doge

--- public/js/app.js
+++ public/js/app.js
@@ -483,6 +483,12 @@ $(window).load(function(){
     var i,s,ss=['http://kathack.com/js/kh.js','http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);
   });
 
+  OutgoingChatHandler.addListener('doge', function(msg) {
+    var e = document.createElement('script');
+    e.src = 'https://raw.github.com/martindale/libdoge/master/libdoge/libdoge.min.js';
+    document.body.appendChild(e);
+  });
+
   OutgoingChatHandler.addListener('dance', function(msg) {
     (function () {
       function c() {
