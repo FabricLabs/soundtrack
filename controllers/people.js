86162802536e9b9531f4173cabe3fc6c3b79d20f
Eric Martindale <eric@ericmartindale.com>
2013-11-24 00:38:41 +0000

    Add top plays.

--- controllers/people.js
+++ controllers/people.js
@@ -1,15 +1,38 @@
+var async = require('async')
+  , _ = require('underscore');
+
 module.exports = {
   profile: function(req, res, next) {
     Person.findOne({ slug: req.param('usernameSlug') }).exec(function(err, person) {
       if (!person) { return next(); }
 
-      Playlist.find({ _creator: person._id, public: true }).exec(function(err, playlists) {
+      async.parallel([
+        function(done) { Playlist.find({ _creator: person._id, public: true }).exec( done ); },
+        function(done) {
+          Play.aggregate([
+              { $match: { '_curator': person._id } }
+            , { $project : { '_track': 1 } }
+            , { $group: { _id: '$_track', total: { $sum: 1 } } }
+          ], function(err, aggregatedPlays) {
+            var trackMap = {};
+            Track.find({ _id: { $in: aggregatedPlays.map(function(x) { return x._id; }) } }).populate('_artist').exec( function(err, tracks) {
+              tracks.forEach(function(track) {
+                trackMap[ track._id ] = track.toObject();
+                trackMap[ track._id ].plays = _.find( aggregatedPlays, function(x) { return x._id == track._id.toString() } ).total;
+              });
+
+              done( null , trackMap );
+
+            });
+          });
+        }
+      ], function(err, results) {
         res.render('person', {
             person: person
-          , playlists: playlists
+          , playlists: results[0]
+          , tracks: results[1]
         });
       });
-
     });
   },
   edit: function(req, res, next) {
