88efa1e546766cca9d5a32a92b65bd624fd5339f
Nate Richardson <nate@naterichardson.com>
2014-02-18 14:33:37 -0500

    fixed parameters for skip condition
    wasn't matching

--- public/js/app.js
+++ public/js/app.js
@@ -667,7 +667,7 @@ $(window).load(function(){
 
   $(document).on('click', '*[data-action=skip-track]', function(e) {
     e.preventDefault();
-    if (!confirm("Are you sure you want to skip?")) = true {
+    if (!confirm("Are you sure you want to skip?") === true) {
       soundtrack.player.pause();
       $.post('/skip');
       return false;
