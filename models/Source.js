4441410c57e646e1c7f9c20057fe49e6df8bc35b
Eric Martindale <eric@ericmartindale.com>
2014-06-12 04:15:54 +0000

    Initial implementation of the Source schema.

--- models/Source.js
+++ models/Source.js
@@ -2,16 +2,34 @@ var mongoose = require('mongoose')
   , Schema = mongoose.Schema
   , ObjectId = mongoose.SchemaTypes.ObjectId;
 
+var config = require('../config');
+
 // this defines the fields associated with the model,
 // and moreover, their type.
 var SourceSchema = new Schema({
-    id:    { type: String }
-  , type:  { type: String, enum: ['audio/mp3', 'video/youtube'] }
-  , uri:   { type: String }
-  , start: { type: Number }
+    id:   { type: String, required: true, unique: true }
+  , type:  { type: String, enum: ['audio/mp3', 'video/youtube', 'video/mp4'] }
+  , start: { type: Number, default: 0 }
   , end:   { type: Number }
-  , live:  { type: Boolean }
-  , nsfw:  { type: Boolean }
+  , flags: {
+        live:  { type: Boolean, default: false } // ~bad audio
+      , nsfw:  { type: Boolean, default: false } // ~bad video
+    }
+});
+
+SourceSchema.virtual('uri').get(function() {
+  var parts = id.split(':');
+  switch (source) {
+    case 'youtube':
+      return 'https://www.youtube.com/watch?v=' + parts[1];
+    break;
+    case 'soundcloud':
+      return 'https://api.soundcloud.com/tracks/' + parts[1] + '?clientID=' + config.soundcloud.id;
+    break;
+    default:
+      return undefined;
+    break;
+  }
 });
 
 var Source = mongoose.model('Source', SourceSchema);
