2d0c9b5bd4fc664b4cdc8fcee9d5e3e154e8ae8a
Eric Martindale <eric@ericmartindale.com>
2015-01-20 16:28:51 -0500

    Use safe parameter.

--- controllers/people.js
+++ controllers/people.js
@@ -5,69 +5,14 @@ module.exports = {
   profile: function(req, res, next) {
     Person.findOne({ slug: req.param('usernameSlug') }).exec(function(err, person) {
       if (!person) { return next(); }
-
+      
       var LIMIT = 50;
-
+      
       async.parallel([
-        function(done) {
-          var q = { _creator: person._id };
-
-          if (!req.user || req.user._id.toString() !== person._id.toString()) {
-            q.public = true;
-          }
-
-          Playlist.find( q ).sort('-_id').populate('_tracks').exec(function(err, playlists) {
-            done( err , playlists );
-          });
-        },
-        function(done) {
-          Play.find({ _curator: person._id }).sort('-timestamp').limit(20).populate('_track _curator').exec(function(err, plays) {
-            Artist.populate( plays , {
-              path: '_track._artist _track._credits'
-            }, done );
-          });
-        },
-        function(done) {
-          async.parallel([
-            function(complete) {
-              Play.aggregate([
-                { $match: {
-                  _curator: person._id
-                } },
-                { $group: { _id: '$_track', count: { $sum: 1 } } },
-                { $sort: { 'count': -1 } },
-                { $limit: LIMIT }
-              ], function(err, collected) {
-                Track.find({ _id: { $in: collected.map(function(x) { return x._id; }) } }).populate('_artist').exec(function(err, input) {
-                  var output = [];
-                  for (var i = 0; i < collected.length; i++) {
-                    output.push( _.extend( collected[i] , input[i] ) );
-                  }
-                  complete( err , output );
-                });
-              } );
-            },
-            function(complete) {
-              Play.aggregate([
-                { $match: {
-                  _curator: person._id,
-                  timestamp: { $gte: new Date((new Date()) - 30 * 24 * 3600 * 1000) }
-                } },
-                { $group: { _id: '$_track', count: { $sum: 1 } } },
-                { $sort: { 'count': -1 } },
-                { $limit: LIMIT }
-              ], function(err, collected) {
-                Track.find({ _id: { $in: collected.map(function(x) { return x._id; }) } }).populate('_artist').exec(function(err, input) {
-                  var output = [];
-                  for (var i = 0; i < collected.length; i++) {
-                    output.push( _.extend( collected[i] , input[i] ) );
-                  }
-                  complete( err , output );
-                });
-              } );
-            }
-          ], done );
-        }
+        collectUserPlaylists,
+        collectUserPlays,
+        collectPlayStats,
+        collectArtistData
       ], function(err, results) {
 
         var playlists = results[0];
@@ -92,8 +37,121 @@ module.exports = {
                 return x._artist;
               })
             }
+          , artist: (results[3]) ? results[3].artist : null
+          , tracks: (results[3]) ? results[3].tracks : null
         });
       });
+
+      function collectUserPlays(done) {
+        Play.find({ _curator: person._id }).sort('-timestamp').limit(20).populate('_track _curator').exec(function(err, plays) {
+          Artist.populate( plays , {
+            path: '_track._artist _track._credits'
+          }, done );
+        });
+      }
+      
+      function collectUserPlaylists(done) {
+        var q = { _creator: person._id };
+
+        if (!req.user || req.user._id.toString() !== person._id.toString()) {
+          q.public = true;
+        }
+
+        Playlist.find( q ).sort('-_id').populate('_tracks').exec(function(err, playlists) {
+          done( err , playlists );
+        });
+      }
+      
+      function collectPlayStats(done) {
+        async.parallel([
+          function(complete) {
+            Play.aggregate([
+              { $match: {
+                _curator: person._id
+              } },
+              { $group: { _id: '$_track', count: { $sum: 1 } } },
+              { $sort: { 'count': -1 } },
+              { $limit: LIMIT }
+            ], function(err, collected) {
+              Track.find({ _id: { $in: collected.map(function(x) { return x._id; }) } }).populate('_artist').exec(function(err, input) {
+                var output = [];
+                for (var i = 0; i < collected.length; i++) {
+                  output.push( _.extend( collected[i] , input[i] ) );
+                }
+                complete( err , output );
+              });
+            } );
+          },
+          function(complete) {
+            Play.aggregate([
+              { $match: {
+                _curator: person._id,
+                timestamp: { $gte: new Date((new Date()) - 30 * 24 * 3600 * 1000) }
+              } },
+              { $group: { _id: '$_track', count: { $sum: 1 } } },
+              { $sort: { 'count': -1 } },
+              { $limit: LIMIT }
+            ], function(err, collected) {
+              Track.find({ _id: { $in: collected.map(function(x) { return x._id; }) } }).populate('_artist').exec(function(err, input) {
+                var output = [];
+                for (var i = 0; i < collected.length; i++) {
+                  output.push( _.extend( collected[i] , input[i] ) );
+                }
+                complete( err , output );
+              });
+            } );
+          }
+        ], done );
+      }
+            
+      function collectArtistData( artistComplete ) {
+        Artist.findOne({ $or: [
+            { slug: req.param('usernameSlug') }
+          , { slugs: req.param('usernameSlug') }
+        ] }).exec(function(err, artist) {
+          if (!artist) return artistComplete();
+          
+          // handle artist renames
+          if (req.param('usernameSlug') !== artist.slug) {
+            return res.redirect('/' + artist.slug);
+          }
+
+          Track.find({ $or: [
+              { _artist: artist._id }
+            , { _credits: artist._id }
+          ] }).populate('_artist').exec(function(err, tracks) {
+
+            Play.aggregate([
+              { $match: { _track: { $in: tracks.map(function(x) { return x._id; }) } } },
+              { $group: { _id: '$_track', count: { $sum: 1 } } },
+              { $sort: { 'count': -1 } }
+            ], function(err, trackScores) {
+              
+              console.log( 'ARTIST CALLBACK', artist );
+              
+              artistComplete( null , {
+                  artist: artist
+                , tracks: tracks.map(function(track) {
+                    var plays = _.find( trackScores , function(x) { return x._id.toString() == track._id.toString() } );
+                    track.plays = (plays) ? plays.count : 0;
+                    return track;
+                  }).sort(function(a, b) {
+                    return b.plays - a.plays;
+                  })
+              });
+            });
+
+            /* req.soundtrack._jobs.enqueue('artist:update', {
+                id: artist._id
+              , timeout: 3 * 60 * 1000
+            }, function(err, job) {
+              console.log('update artist queued');
+            }); */
+
+          });
+        });
+      }
+      
     });
   },
   mentions: function(req, res, next) {
