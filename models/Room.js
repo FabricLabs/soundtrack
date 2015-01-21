e6a06e72169d0f3965331783ba561a88012b8c65
Eric Martindale <eric@ericmartindale.com>
2015-01-21 14:56:46 -0500

    Add duration to last.fm scrobbles and nowListening.

--- models/Room.js
+++ models/Room.js
@@ -311,6 +311,7 @@ RoomSchema.methods.scrobbleActive = function(requestedTrack, cb) {
         lastfm.track.scrobble({
             artist: track._artist.name
           , track: track.title
+          , duration: track.duration
           , timestamp: Math.floor((new Date()).getTime() / 1000)
         }, function(err, scrobbles) {
           if (err) { return console.log('le fail...', err); }
@@ -352,6 +353,7 @@ RoomSchema.methods.setListeningActive = function(requestedTrack, cb) {
         lastfm.track.updateNowPlaying({
             artist: track._artist.name
           , track: track.title
+          , duration: track.duration
         }, function(err, scrobbles) {
           if (err) { return console.log('le fail...', err); }
 
