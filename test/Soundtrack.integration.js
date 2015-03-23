c61945cf69e7085b961eebe3813cbf7240660896
Eric Martindale <eric@ericmartindale.com>
2015-03-23 01:12:09 -0400

    add basic tests for startup... delay. :(

--- test/Soundtrack.integration.js
+++ test/Soundtrack.integration.js
@@ -1,4 +1,7 @@
 var assert = require('assert');
+var rest = require('restler');
+
+var config = require('../config');
 
 before(function() {
   var app = require('../soundtrack');
@@ -8,6 +11,15 @@ describe('Soundtrack', function() {
   describe('web server', function() {
     it('should run', function() {
       assert('ok');
+    });
+    
+    it('should accept connections', function( done ) {
+      setTimeout(function() {
+        rest.get('http://localhost:'+ config.app.port ).on('complete', function(data, response) {
+          assert( response.statusCode , 200 );
+          done();
+        });
+      }, 1000);
     });
   });
 });
